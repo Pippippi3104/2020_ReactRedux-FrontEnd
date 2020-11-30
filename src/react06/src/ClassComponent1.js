1d193f54cdd749268a2c5289e35cade33289396f
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 18:47:47 +0900

    # finish editing lesson14

---
+++ src/react06/src/ClassComponent1.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//define class by export defalt
+export default class ClassComponent1 extends React.Component {
+    render() {
+        return <div>ClassComponent1</div>;
+    }
+}
--- src/react06/src/index.js
+++ src/react06/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//patarns of export default
+import ClassComponent1 from "./ClassComponent1";
+import ClassComponent2 from "./ClassComponent2";
+
+//patarns of not export default
+import { ClassComponent3 } from "./ClassComponent3";
+
+render(
+  <div>
+    <ClassComponent1 />
+    <ClassComponent2 />
+    <ClassComponent3 />
+  </div>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
