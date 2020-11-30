c96dbe0b8277c5f823bd8c35e3cab057840c4add
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 18:18:14 +0900

    # note the progress - 02

--- src/react05/src/index.js
+++ src/react05/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const var1 = "string";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(var1);
+
+const App = () => (
+  <div>
+    <h2>Start editing to see some magic happen {"\u2728"}</h2>
+  </div>
+)
