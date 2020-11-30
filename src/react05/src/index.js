8e7defc9ee2c7369871a3d15da6b7fa9f3e51c82
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 18:27:15 +0900

    # finish editing lesson13

--- src/react05/src/index.js
+++ src/react05/src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 
-import { var1, function1 } from "./module";
+import { var1, function1, ReactComponent } from "./module";
 
 console.log(var1);
 function1();
 
-const App = () => (
-  <div>
-    <h2>Start editing to see some magic happen {"\u2728"}</h2>
-  </div>
-)
+render(<ReactComponent />, document.getElementById("root"));
