e58c845a0f7f4653cdd3e1318c752fc7e8aca346
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 18:00:34 +0900

    # finish editing lesson12

--- src/react04/src/index.js
+++ src/react04/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 
-const ReturnReactElement = (props) => {
-  console.log(props);
-  return <h2>{props.name} love {props.music}</h2>;
+const ReturnReactElement = ({name, music}) => {
+  return <h2>{name} love {music}</h2>;
 }
 
 render(<ReturnReactElement name="nakanishi" music="JAZZ" />, document.getElementById("root")); 
