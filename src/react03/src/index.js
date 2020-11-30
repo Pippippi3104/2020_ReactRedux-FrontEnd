deafcebfc054a8e26eb608eaa509cf6af53c37a4
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-11-30 17:45:53 +0900

    # finish editing lesson11

--- src/react03/src/index.js
+++ src/react03/src/index.js
@@ -5,4 +5,13 @@ const returnReactElement = () => {
   return <h2>text</h2>
 }
 
-render(returnReactElement(), document.getElementById("root"));
+const returnReactElement2 = () => <h2>text2</h2>
+
+const returnReactElement3 = (hello) => <h2>{hello}</h2>
+
+const returnReactElement4 = (no, name) => {
+  const newStrings = name + "'s number is" + no + "."
+  return <h2>{newStrings}</h2>
+}
+
+render(returnReactElement4("#1", "nakanishi"), document.getElementById("root"));
