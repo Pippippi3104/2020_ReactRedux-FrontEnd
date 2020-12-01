6e5cf36b2e94c2461d0e90eef93b15ecdcef7cab
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-12-02 00:53:01 +0900

    # finish editing lesson17

--- src/react08/src/index.js
+++ src/react08/src/index.js
@@ -3,7 +3,12 @@ class Human {
     this.name = name;
     this.age = age;
   }
+
+    callMyProfile() {
+      console.log(this.name, this.age);
+  }
 };
 
 const Nakanishi = new Human("Nakanishi", 30);
-console.log(Nakanishi);
+//console.log(Nakanishi);
+Nakanishi.callMyProfile();
