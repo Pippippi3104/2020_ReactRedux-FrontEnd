1523a14644b0372867d3735050a5003f754f0631
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-02 23:58:22 +0900

    # note the progress - 02

--- src/react09/src/index.js
+++ src/react09/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Human extends React.Component{
+  constructor(props) {
+    super(props)
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  render() {
+    return <h2>test</h2>;
+  }
+}
+
+render(<Human />, document.getElementById("root"));
+
