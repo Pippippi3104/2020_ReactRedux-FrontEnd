5f44f7c159fee1a1be7c4b747a494cffd272b6f5
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-05 13:32:26 +0900

    # note the progress - 02

--- src/filter01/src/index.js
+++ src/filter01/src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const array1 = [1, 2, 3, 4, 5];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const newArray1 = array1.filter((output, index) => {
+  return output > 3
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(newArray1);
