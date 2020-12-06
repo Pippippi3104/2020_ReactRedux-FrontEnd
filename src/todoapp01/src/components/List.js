957770f28aaf5abf4063a9a2a8f0ba2cd6f0b64e
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-06 15:16:00 +0900

    # note the ptogress - 02

--- src/todoapp01/src/components/List.js
+++ src/todoapp01/src/components/List.js
@@ -6,9 +6,17 @@ export class List extends React.Component {
     }
 
     render() {
+        const list = this.props.todos.map(todo => {
+            return (
+                <li>
+                    #{todo.id} {todo.title}
+                </li>
+            );
+        });
         return (
             <div>
                 <h2>List</h2>
+                <ul>{list}</ul>
             </div>
         );
     }
