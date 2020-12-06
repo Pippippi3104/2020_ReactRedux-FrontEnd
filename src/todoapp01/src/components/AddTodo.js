61cb296fb1961d13442983ba677982ff8cca3fd5
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-06 15:35:47 +0900

    # note the progress - 01

--- src/todoapp01/src/components/AddTodo.js
+++ src/todoapp01/src/components/AddTodo.js
@@ -3,13 +3,22 @@ import React from "react";
 export class AddTodo extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { title: "test" }
     }
 
     render() {
         return (
             <div>
                 <h2>AddTodo</h2>
+                <form>
+                    <input value={this.state.title} onChange={this.handleChange} />
+                    <input type="submit" value="Add to todo list" />
+                </form>
             </div>
         );
+    }
+
+    handleChange = () => {
+        alert("change");
     }
 }
