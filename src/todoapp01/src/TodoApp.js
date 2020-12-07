e9923a454d49315172e186a37ca0bebc31bf832f
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-07 23:37:20 +0900

    # note the progress - 01

--- src/todoapp01/src/TodoApp.js
+++ src/todoapp01/src/TodoApp.js
@@ -22,9 +22,13 @@ export class TodoApp extends React.Component {
         return (
             <div>
                 <h2>TodoApp</h2>
-                <AddTodo />
+                <AddTodo addTodo={this.addTodo}/>
                 <List todos={this.state.todos} />
             </div>
         );
+    }
+
+    addTodo = () => {
+        this.setState({ todos: [...this.state.todos, { id: 100, title: "new" }] });
     }
 }
