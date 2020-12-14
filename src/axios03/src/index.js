95bfff8f76e1bbb1385af45e11fbd354c91abbed
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-15 00:10:59 +0900

    # finish editing lesson41

--- src/axios03/src/index.js
+++ src/axios03/src/index.js
@@ -6,18 +6,45 @@ import axios from "axios";
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {member: []}
+  }
+
+  memberList(list) {
+    const memberList = list.map((member, index) => {
+      console.log(index);
+      console.log(member.name);
+      console.log(member.age);
+      return (
+        <li>
+          {member.name} {member.age}
+          aaa
+        </li>
+      );
+    });
   }
 
   render() {
-    return <button onClick={this.getJson}>Get Json</button>
+    console.log(this.state.member);
+    return (
+      <div>
+        <button onClick={this.getJson}>Get Json</button>
+        {this.memberList(this.state.member)}
+      </div>
+    )
   }
 
   getJson = () => {
-    const url = "https://jsonplaceholder.typicode.com/todos";
+    /* const url = "https://jsonplaceholder.typicode.com/todos";
 
     axios.get(url).then(res => {
-      console.log(res.data);
-    });
+      console.log(res.data[0]);
+      const array = {member: res.data};
+      this.setState(array);
+    }); */
+
+    var array = require( "./data.json" );
+    console.log(array);
+    this.setState(array);
   }
 }
 
