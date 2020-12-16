297ab3c960487e0d283f62a07412f5b20e32c111
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-16 11:15:15 +0900

    # finish editing lesson43

--- src/giphy01/src/index.js
+++ src/giphy01/src/index.js
@@ -8,6 +8,14 @@ const url = "https://api.giphy.com/v1/gifs/search?q="+ search +"&api_key="+ key 
 
 axios.get(url).then(res => {
   console.log(res.data);
+
+  const data = res.data.data;
+  const imageUrl = data[0].images.downsized.url;
+  console.log(imageUrl)
+
+  const img = document.createElement("img");
+  img.src = imageUrl;
+  document.body.appendChild(img);
 });
 
 
