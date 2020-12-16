47d7c3942529938db3fcbedd823fd69910ba876e
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-16 11:30:31 +0900

    # note the progress - 01

--- src/giphy01/src/index.js
+++ src/giphy01/src/index.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
 import axios from "axios";
 
 const search = "cat";
@@ -6,16 +9,30 @@ const limit = 3;
 
 const url = "https://api.giphy.com/v1/gifs/search?q="+ search +"&api_key="+ key +"&limit=$"+ limit;
 
-axios.get(url).then(res => {
-  console.log(res.data);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {giphyList: []};
+  }
 
-  const data = res.data.data;
-  const imageUrl = data[0].images.downsized.url;
-  console.log(imageUrl)
+  render() {
+    return <div>app</div>
+  }
 
-  const img = document.createElement("img");
-  img.src = imageUrl;
-  document.body.appendChild(img);
-});
+  giphyApi() {
+    axios.get(url).then(res => {
+      console.log(res.data);
+    
+      const data = res.data.data;
+      const imageUrl = data[0].images.downsized.url;
+      console.log(imageUrl)
+    
+      const img = document.createElement("img");
+      img.src = imageUrl;
+      document.body.appendChild(img);
+    });
+  }
+}
 
+render(<App />, document.getElementById("root"));
 
