5f1a9ac12990887c7911105a1f52aa6391402a17
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-17 11:55:19 +0900

    # finish editing lesson56

--- src/redux01/src/containers/App.js
+++ src/redux01/src/containers/App.js
@@ -1,6 +1,8 @@
 import App from "../components/App";
 import { connect } from "react-redux";
 
+import { minus, plus } from "../actions/index";
+
 const mapStateToProps = state => {
     return {
       number: state
@@ -10,10 +12,10 @@ const mapStateToProps = state => {
   const mapDispatchToProps = dispatch => {
     return {
       plus: num => {
-        dispatch({ type: "PLUS", payload: {num: num }});
+        dispatch(plus(num));
       },
       minus: num => {
-        dispatch({ type: "MINUS", payload: {num: num }});
+        dispatch(minus(num));
       }
     };
   };
