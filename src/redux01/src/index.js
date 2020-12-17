7bc67997c38b319d908d2dbace312a793aa089a3
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-17 10:49:56 +0900

    # note the progress - 01

--- src/redux01/src/index.js
+++ src/redux01/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "PLUS_ONE":
+      return state + 1;
+  
+    default:
+      return state;
+  }
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer);
+
+console.log(store);
+
