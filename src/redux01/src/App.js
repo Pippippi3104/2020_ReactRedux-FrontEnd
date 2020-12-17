feffa50ac8e5cdd3ab8cd9ea071b48f69e02af95
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-17 11:33:36 +0900

    # note the progress - 01

--- src/redux01/src/App.js
+++ src/redux01/src/App.js
@@ -1,6 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const App = () => <h2>App</h2>;
+const App = ({ number, plus, minus }) =>
+  <h2 onClick={() => {
+    plus(10);
+  }}>
+    App{number}
+  </h2>;
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    number: state
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    plus: num => {
+      dispatch({ type: "PLUS", payload: {num: num }});
+    },
+    minus: num => {
+      dispatch({ type: "MINUS", payload: {num: num }});
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
