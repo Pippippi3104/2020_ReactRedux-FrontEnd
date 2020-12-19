5604e8a3fd2733377431db99c3ca9648e97bd5d4
Satoshi Shimamura <shimamura.satoshi.jri@outlook.jp>
2020-12-19 16:15:05 +0900

    # note the progress - 01

---
+++ src/final01/src/components/Search.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { title: "test" }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>AddTodo</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input value={this.state.title} onChange={this.handleChange} />
+                    <input type="submit" value="Add to todo list" />
+                </form>
+            </div>
+        );
+    }
+
+    handleChange = event => {
+        const title = event.target.value;
+        console.log(title);
+
+        this.setState({ title: title });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.addTodo(this.state.title);
+        this.setState({ title: "" });
+    }
+}
+
+export default Search;
